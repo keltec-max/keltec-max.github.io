5a8ac70188c9a3feb6448b9a92747c0e633617c0
Kelvin <>
2026-05-11 23:45:27 -0300

    AtualizaÃ§Ã£o do site

---
+++ update2.ps1
@@ -0,0 +1,11 @@
+```powershell id="g4h0jx"
+cd "C:\Users\kelvi\OneDrive\Documentos\Site"
+
+git add .
+git commit -m "Atualização do site"
+git push
+
+Write-Host ""
+Write-Host "🚀 Site atualizado!"
+Write-Host "🌐 https://keltec-max.github.io/"
+```
